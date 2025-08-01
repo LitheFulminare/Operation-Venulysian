f9d89c1972758e664e08e3bb019b07b574943395
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-07-31 21:11:52 -0300

    create movement curve
    used path2d to create a curve to the destination

--- Operation Venulysian/Player/grappling_hook_controller.gd
+++ Operation Venulysian/Player/grappling_hook_controller.gd
@@ -1,6 +1,9 @@
 class_name GrapplingHookComponent
 extends Node
 
+@export var player: Player
+@export var path_2d: Path2D
+
 var current_grappling_hook_position: Vector2
 
 func _process(delta: float) -> void:
@@ -10,5 +13,7 @@ func _process(delta: float) -> void:
 func use_grappling_hook():
 	if PlayerVars.is_in_grappling_range:
 		print("Player destination: " + str(current_grappling_hook_position))
+		path_2d.curve.set_point_position(0, player.global_position)
+		path_2d.curve.set_point_position(1, current_grappling_hook_position)
 	else:
 		print("Player used grappling hook outside of range")
