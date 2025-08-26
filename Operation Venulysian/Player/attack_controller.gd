0955fd6bdec187248ce132c726562081c9a085f5
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-08-26 19:45:51 -0300

    progress melee attack
    added area2d to the controller and began to program its interactions

--- Operation Venulysian/Player/attack_controller.gd
+++ Operation Venulysian/Player/attack_controller.gd
@@ -1,21 +1,34 @@
 class_name AttackController
 extends Node
 
+@export_group("Components")
 @export var player: Player
 @export var attack_window_timer: Timer
+@export var sword_hitbox: Area2D
+## Controls for how long the sword hitbox will be active looking for collision
+@export var sword_hit_window_timer: Timer
+
+@export_group("Parameters")
 @export var damage: float = 10
 ## How much time the player has to chain one attack into the other (in seconds)
 @export var combo_window: float = 0.5
+## Controls for how long the sword hitbox will be active looking for collision
+@export var sword_hit_window: float = 0.2
 
 var is_combo_window_active: bool = false
 var current_attack: int = 1
 
 func _ready() -> void:
 	attack_window_timer.wait_time = combo_window
+	#sword_hit_window.waittime
+	sword_hitbox.monitorng = false
 
 func melee_attack():
 	# First I should check if the player is in the middle of the animation,
 	# but I don't have that now
+	
+	sword_hitbox.monitoring = true
+	sword_hit_window_timer.start()
 	
 	match current_attack:
 		1:
@@ -33,7 +46,10 @@ func melee_attack():
 		current_attack += 1
 		attack_window_timer.start()
 
-
 func _on_combo_window_timer_timeout() -> void:
 	is_combo_window_active = false
 	current_attack = 1
+
+
+func _on_sword_hit_window_timeout() -> void:
+	sword_hitbox.monitoring = false
