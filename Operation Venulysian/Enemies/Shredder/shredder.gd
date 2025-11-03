1c2297def6cc151951b7b346859291bfb1b4582f
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-11-03 19:24:14 -0300

    progress boids

--- Operation Venulysian/Enemies/Shredder/shredder.gd
+++ Operation Venulysian/Enemies/Shredder/shredder.gd
@@ -1,12 +1,25 @@
 class_name Shredder extends Area2D
 
 var rays: Array[RayCast2D]
+var boids_in_sight: Array[Shredder]
+var velocity: Vector2 = Vector2.ZERO
+var speed: float = 7.0
+var screen_size: Vector2
 
 func _ready() -> void:
 	rays = get_rays()
+	screen_size = get_viewport_rect().size
+	randomize()
 	
 func get_rays() -> Array[RayCast2D]:
 	var rayArray: Array[RayCast2D]
 	for ray in $"Raycast folder".get_children():
 		rayArray.append(ray)
 	return rayArray
+
+func _process(delta: float) -> void:
+	boids()
+	checkCollision()
+	velocity = velocity.normalized() * speed
+	move()
+	rotation = lerp_angle(rotation, velocity.angle_to_point(Vector2.ZERO), 0.4)
